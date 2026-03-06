d383cacecb20afd31b86f13b131eca19fd7d1ddb
RyuuShiro30 <annoraegaprasetyo@gmail.com>
2026-03-06 14:20:07 +0700

    Testing

--- testing/lib/main.dart
+++ testing/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -102,9 +102,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Hai Saya Belajar Flutter'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
